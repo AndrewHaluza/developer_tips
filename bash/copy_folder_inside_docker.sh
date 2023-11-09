3a6f5470c27d540372a1993b662b79951e305acf
ND <nd@newerest.space>
2023-11-09 14:31:42 +0200

    fix: chmod +x; add --sudo flag;

--- bash/copy_folder_inside_docker.sh
+++ bash/copy_folder_inside_docker.sh
@@ -1,10 +1,48 @@
 #!/bin/sh
 
-ARG_FROM=${1}
-ARG_TO=${2}
 
-# copping itself
-sudo docker cp $ARG_FROM $ARG_TO
+# Initialize variables with default values
+ARG_FROM=""
+ARG_TO=""
 
+WITH_SUDO=false
+
+# Process command line arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --sudo)
+            WITH_SUDO=true
+            ;;
+        *)
+            if [ -z "$ARG_FROM" ]; then
+                ARG_FROM="$1"
+            elif [ -z "$ARG_TO" ]; then
+                ARG_TO="$1"
+            elif [ -z "$ARG_FILE_PATH" ]; then
+                ARG_FILE_PATH="$1"
+            else
+                echo "Ignoring extra argument: $1"
+            fi
+            ;;
+    esac
+    shift
+done
+
+# Check if any of the required arguments are missing
+if [ -z "$ARG_FROM" ] || [ -z "$ARG_TO" ]; then
+    echo "Usage: $0 <from> <to> [--sudo]"
+    exit 1
+fi
+
+# Copy command
+COMMAND_COPY="docker cp $ARG_FROM $ARG_TO"
 # assign copping permissions
-sudo chmod -R 775 $ARG_TO
+COMMAND_CHMOD="chmod -R 775 $ARG_TO"
+
+if [ $WITH_SUDO = true ]; then
+    sudo bash -c "$COMMAND_COPY"
+    sudo bash -c "$COMMAND_CHMOD"
+else
+    bash -c "$COMMAND_COPY"
+    bash -c "$COMMAND_CHMOD"
+fi
--- bash/replace_file_content.sh
+++ bash/replace_file_content.sh
@@ -1,5 +1,45 @@
-ARG_OLD_TEXT=${1}
-ARG_NEW_TEXT=${2}
-ARG_FILE_PATH=${3}
+#!/bin/bash
 
-sed -i "s/$ARG_OLD_TEXT/$ARG_NEW_TEXT/" "$ARG_FILE_PATH"
+# Initialize variables with default values
+ARG_OLD_TEXT=""
+ARG_NEW_TEXT=""
+ARG_FILE_PATH=""
+WITH_SUDO=false
+
+# Process command line arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --sudo)
+            WITH_SUDO=true
+            ;;
+        *)
+            if [ -z "$ARG_OLD_TEXT" ]; then
+                ARG_OLD_TEXT="$1"
+            elif [ -z "$ARG_NEW_TEXT" ]; then
+                ARG_NEW_TEXT="$1"
+            elif [ -z "$ARG_FILE_PATH" ]; then
+                ARG_FILE_PATH="$1"
+            else
+                echo "Ignoring extra argument: $1"
+            fi
+            ;;
+    esac
+    shift
+done
+
+# Check if any of the required arguments are missing
+if [ -z "$ARG_OLD_TEXT" ] || [ -z "$ARG_NEW_TEXT" ] || [ -z "$ARG_FILE_PATH" ]; then
+    echo "Usage: $0 <old_text> <new_text> <file_path> [--sudo]"
+    exit 1
+fi
+
+# Copy command
+COMMAND="sed -i \"s/$ARG_OLD_TEXT/$ARG_NEW_TEXT/\" \"$ARG_FILE_PATH\""
+
+if [ $WITH_SUDO = true ]; then
+    sudo bash -c "$COMMAND"
+else
+    bash -c "$COMMAND"
+fi
+
+echo "Updated $ARG_OLD_TEXT to $ARG_NEW_TEXT in $ARG_FILE_PATH. (ARG_WITH_SUDO: $WITH_SUDO)"
